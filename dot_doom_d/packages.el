b8f771d7b5f043eba0a886ecaa5167bbf57eac50
Kha Dinh <dalo2903@gmail.com>
2021-03-25 22:47:51 +0900

    .

---
+++ dot_doom_d/packages.el
@@ -0,0 +1,23 @@
+(package! nyan-mode)
+(package! shfmt)
+
+(package! doct)
+(package! org-pretty-tags)
+(package! org-roam-server)
+(package! gnuplot-mode)
+(package! gnuplot)
+;; (package!  ivy-bibtex)
+(package! org-roam-bibtex)
+(package! org-ref)
+(package! org-pomodoro)
+(package! org-tree-slide)
+;; (package! org-download)
+
+(package! lsp-latex)
+(unpin! pdf-tools)
+;; (package! pdf-tools :pin nil)
+
+(package! comment-dwim-2)
+(package! iedit)
+
+(package! zygospore)
